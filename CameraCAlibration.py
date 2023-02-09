1f943fada35e3162be8b1c1fc6c0fc6e77c38c0b
Sjoerd Schilder <schildersjoerd@gmail.com>
2023-02-09 18:11:10 +0100

    only manual when automatic does not work

--- CameraCAlibration.py
+++ CameraCAlibration.py
@@ -2,25 +2,10 @@ import numpy as np
 import cv2 as cv
 import glob
 
-
-def click_event(event, x, y, flags, params):
-    if event == cv.EVENT_LBUTTONDOWN:
-        print(x, ' ', y)
-        params.append([x, y])
-        if len(params) == 4:
-            cv.destroyWindow('img')
-
-
-def getChessboardCorners(img):
-    cv.imshow("img", img)
-    corners = []
-    cv.setMouseCallback('img', click_event, param= corners)
-    cv.waitKey(0)
-    print(corners)
-    return True, corners
-
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+board_shape = (7, 10)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((6*7,3), np.float32)
@@ -32,13 +17,34 @@ imgpoints = [] # 2d points in image plane.
 
 images = glob.glob('images/chessImage?.jpg')
 
+def click_event(event, x, y, flags, params):
+    if event == cv.EVENT_LBUTTONDOWN:
+        print(x, ' ', y)
+        params.append([x, y])
+        (c, r) = board_shape
+        if len(params) == (c+1) * (r+1):
+            cv.destroyWindow('img')
+
+
+def getChessboardCorners(img):
+    cv.imshow("img", img)
+    corners = []
+    cv.setMouseCallback('img', click_event, param= corners)
+    cv.waitKey(0)
+    print(corners)
+    return True, corners
+
+
+
 def main():
     for fname in images:
         print(fname)
         img = cv.imread(fname)
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         # Find the chess board corners
-        ret, corners = getChessboardCorners(gray)
+        ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+        if not ret:
+            ret, corners = getChessboardCorners(gray)
         # If found, add object points, image points (after refining them)
         objpoints.append(objp)
         corners2 = cv.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
