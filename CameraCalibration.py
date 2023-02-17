b776e630a28a3f76ff52ace42729a3ed4a10999a
Sjoerd Schilder <schildersjoerd@gmail.com>
2023-02-17 14:24:46 +0100

    comments and refactoring

--- CameraCalibration.py
+++ CameraCalibration.py
@@ -46,17 +46,18 @@ def draw_cube(img, imgpts):
     return img
 
 
-# generates an new image from img, with cornerpoints and a cube projected on to it
-def generateImage(img, calibration, corners = None):
+# generates a new image from img, with cornerpoints and a cube projected on to it
+def Online(img, calibration, corners = None):
 
-    # #prepare
-    # objp = np.zeros((columns * rows, 3), np.float32)
-    # objp[:, :2] = np.mgrid[0:columns*cube_size:cube_size, 0:rows*cube_size:cube_size].T.reshape(-1, 2)
-    axis = np.float32([[0, 0, 0], [0, 2*cube_size, 0], [2*cube_size, 2*cube_size, 0], [2*cube_size, 0, 0],
-                       [0, 0, -2*cube_size], [0, 2*cube_size, -2*cube_size], [2*cube_size, 2*cube_size, -2*cube_size], [2*cube_size, 0, -2*cube_size]])
+    # the 3d points of a cube. 2 * cube_size to make the cube 2 chessboard squares sized
+    length = 2 * cube_size
+    cube = np.float32([[0, 0, 0], [0, length, 0], [length, length, 0], [length, 0, 0],
+                       [0, 0, -length], [0, length, -2 * cube_size], [length, length, -length], [length, 0, -length]])
 
+    # the camera calibration matrices
     ret, mtx, dist, rvecs, tvecs = calibration
 
+    #gray the image
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
     #find corners
@@ -65,34 +66,42 @@ def generateImage(img, calibration, corners = None):
         img = cv.drawChessboardCorners(img, board_shape, corners, ret)
 
     if ret is not False:
+        # refine corners
         corners2 = cv.cornerSubPix(gray, corners, (5, 5), (-1, -1), criteria)
 
         # find the rotation and translation vectors.
         ret, rvecs, tvecs = cv.solvePnP(objp, corners2, mtx, dist)
 
-        imgpts, jac = cv.projectPoints(axis, rvecs, tvecs, mtx, dist)
+        #project the real cube coordinates to image coordinates
+        imgpts, jac = cv.projectPoints(cube, rvecs, tvecs, mtx, dist)
 
+        #draw the cube using the coordinates
         img = draw_cube(img, imgpts)
 
     return img
 
 
-def Online(images, calibration1, calibration2, calibration3):
-    #do the online part of the assignment
-    #draws a cube on each chessboard image and displays it
+# draws a cube using the online function, but using the webcam
+# using the different calibrations
+def realTimeOnline(calibration1, calibration2, calibration3):
 
+    #get the webcam
     vid = cv.VideoCapture(0)
     while True:
         ret_vid, frame = vid.read()
         if not ret_vid:
             print("could not find video input, exiting...")
             break
-        frame = generateImage(frame, calibration1)
+
+        # show the videos of the different calibrations
+        frame = Online(frame, calibration1)
         cv.imshow('vid calibration1', frame)
-        frame = generateImage(frame, calibration2)
+        frame = Online(frame, calibration2)
         cv.imshow('vid calibration2', frame)
-        frame = generateImage(frame, calibration3)
+        frame = Online(frame, calibration3)
         cv.imshow('vid calibration3', frame)
+
+        # abort if escape is pressed
         key = cv.waitKey(1)
         if key % 256 == 27:
             print("aborting")
@@ -126,6 +135,9 @@ def interpolateCorners(init_corners, image):
     return corners
 
 
+# click event handler
+# saves the x and y coordinates of 4 clicks
+# need to in order: top left, clockwise
 def click_event(event, x, y, flags, params):
     global clicks
     corners = params[0]
@@ -139,6 +151,7 @@ def click_event(event, x, y, flags, params):
             cv.destroyWindow('img Click corners')
 
 
+# shows the image where corners need to be manually acquired
 def getChessboardCorners(img):
     cv.imshow('img Click corners', img)
     corners = np.zeros((4, 2))
@@ -146,64 +159,79 @@ def getChessboardCorners(img):
     cv.waitKey(0)
     return True, corners
 
+
+# collect all the cornerpoints of the images in images
+# when corners cant be detected automatically, acquire them manually
+# also refines the corner positions of both automatic and manual corners
 def Offline(images):
 
     # Arrays to store object points and image points from all the images.
     objpoints = []  # 3d point in real world space
     imgpoints = []  # 2d points in image plane.
 
+    # get the image and gray them out
     img = cv.imread(images[0])
-    cv.imshow("img", img)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 
     for fname in images:
         img = cv.imread(fname)
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        # Find the chess board corners
+        # Find the chess board corners automatically
         ret, corners = cv.findChessboardCorners(gray, board_shape, None)
         if not ret:
-            print(fname)
+            #find chessboard corners manually
             ret, corners = getChessboardCorners(gray)
             corners = interpolateCorners(corners, img)
+
+        # refine the corner positions
         corners = cv.cornerSubPix(gray, corners, (5, 5), (-1, -1), criteria)
+
         # If found, add object points, image points (after refining them)
         objpoints.append(objp)
         imgpoints.append(corners)
+
         # Draw and display the corners
         cv.drawChessboardCorners(img, board_shape, corners, ret)
         cv.imshow('img', img)
         cv.waitKey(0)
+
     cv.destroyWindow('img')
+
+    #calibrate the camera using all the points found in all the images
     return cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+# calibrate the camerea using 3 different runs of images
+# then display the test image with a cube
+# then open camera to real time display cube
 def main():
     image = cv.imread(f'{os.getcwd()}\\test_image\\testImageTrue.png')
 
     images = glob.glob(f'images\\chessImage*.png')
     # camera calibration for all images
     calibration1 = Offline(images)
-    img = generateImage(image, calibration1)
+    img = Online(image, calibration1)
     cv.imshow("calibration 1", img)
     cv.imwrite("cubeCalibration1.png", img)
 
     images = glob.glob(f'images2\\chessImage*.png')
     # camera calibration for run 2
     calibration2 = Offline(images)
-    img = generateImage(image, calibration2)
+    img = Online(image, calibration2)
     cv.imshow("calibration 2", img)
     cv.imwrite("cubeCalibration2.png", img)
 
     images = glob.glob(f'images3\\chessImage*.png')
     # camera calibration for run 3
     calibration3 = Offline(images)
-    img = generateImage(image, calibration3)
+    img = Online(image, calibration3)
     cv.imshow("calibration 3", img)
     cv.imwrite("cubeCalibration3.png", img)
 
     cv.waitKey(0)
 
     #Online phase
-    Online(images, calibration2, calibration2, calibration3)
+    realTimeOnline(calibration1, calibration2, calibration3)
 
 if __name__ == "__main__":
     main()
--- GetImages.py
+++ GetImages.py
@@ -4,30 +4,38 @@ import numpy as np
 import cv2 as cv
 import glob
 
+#video input
 vid = cv.VideoCapture(0)
+# chessboard parameters
 columns = 6
 rows = 9
 board_shape = (columns, rows)
 
+# opens webcam to capture images that automatically detects corners is a chessboard
+# prints real time whether they are found automatically
+# space to save current image, esc to quit
 def main():
     i = 0
     trues = 0
     false = 0
+
     print("get 25 images, 5 which do not automatically detect corners.")
     print("Take a final test image that has the chessboard tilted significantly close to the image border. Corners have to be found automatically")
     print("press space to take picture, esc to exit")
+
     while True :
         ret_vid, frame = vid.read()
+
         if not ret_vid:
             print("could not find video input, exiting...")
             break
 
+        # get corners
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         ret, corners = cv.findChessboardCorners(gray, board_shape, flags=cv.CALIB_CB_FAST_CHECK)
         print(ret)
-        # frameCorner = cv.drawChessboardCorners(frame, board_shape, corners, ret)
-        # cv.imshow('img', frameCorner)
-        cv.imshow('img without conrer', frame)
+        
+        cv.imshow('img', frame)
         key = cv.waitKey(1)
 
         if key%256 == 27:
@@ -40,7 +48,7 @@ def main():
             else:
                 false +=1
             print(f"Able to find corners: {ret}, trues: {trues}, false {false}")
-            cv.imwrite(f"test_image\\testImage{ret}.png", frame)
+            cv.imwrite(f"test_image\\chessImage{i}{ret}.png", frame)
             i += 1
 
     vid.release()
